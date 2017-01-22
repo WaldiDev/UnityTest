19b8374415c3cbf5085c817cc40b205d27776718
KeeGordon <ruben.schertler@gmail.com>
2017-01-22 16:52:08 +0100

    Added TestScript

---
+++ Assets/Editor/TestScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class TestScript {
+
+	[Test]
+	public void EditorTest()
+	{
+		//Arrange
+		var gameObject = new GameObject();
+
+		//Act
+		//Try to rename the GameObject
+		var newGameObjectName = "My game object";
+		gameObject.name = newGameObjectName;
+
+        //Assert
+        //The object has a new name
+        Assert.AreNotEqual(newGameObjectName, gameObject.name);
+		//Assert.AreEqual(newGameObjectName, gameObject.name);
+	}
+}
